3af4371ef04bf61450b7528bd1128a6320c8c247
tangled.loops <tangld.loops@gmail.com>
2023-09-27 22:14:47 -0600

    Add nodemailer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import Sidebar from '@/components/ui/sidebar'
 import Image from 'next/image'
 
@@ -6,8 +8,22 @@ export default function Home() {
   return (
     <>
       <Sidebar active={"Home"} />
-      <main className='md:ml-[200px] p-10'>
-        aksdjflksjadlkfjs
+      <main className='md:ml-[200px] p-10 grid grid-cols-2'>
+        <Card className=''>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <div>
+          lsadjflkjsdklf
+        </div>
       </main>
     </>
   )
