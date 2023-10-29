3084f9d81b7b9e28ec9c1252585aeea04baa5989
tangled.loops <tangld.loops@gmail.com>
2023-10-29 17:58:28 -0600

    Try updating to 14 for deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'drizzle-kit';
 const conn = 
   process.env.NODE_ENV === 'production' 
     ? process.env.PRODUCTION_DATABASE_URL
-    : process.env.DATABASE_URL!
+    : process.env.DATABASE_URL
 
 console.log(conn)
 
